d875a3763265303ee240219a91e6c7554987d501
Shuai Wang <wswatch@cs.uchicago.edu>
2019-01-01 14:50:07 -0800

    socket.io update

--- server/server.js
+++ server/server.js
@@ -11,16 +11,25 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-io.on('connection',(socket)=>{
+io.on('connection',function(socket){
   console.log('has a new connection!');
 
-  socket.on('disconnect', ()=>{
+  socket.on('disconnect', function(){
     console.log('disconnect to the client.');
+  });
+
+  socket.emit('newMessage', {
+    from: 'sb',
+    text: 'It is a new Message.',
+    createAt: new Date().getTime()
+  });
+  socket.on('createMessage', function(data) {
+    console.log(data);
   })
 });
 
 app.use(express.static(myPath));
 
-server.listen(port, ()=>{
+server.listen(port, function() {
   console.log('The system start on port', port);
 })
