bf7ce126da5065ae26f37b380b6aed2da4259ae5
Shuai Wang <wswatch@cs.uchicago.edu>
2019-01-01 13:26:29 -0800

    add the socket.io

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
+
 const port = process.env.PORT || 3000;
 const myPath = path.join(__dirname, "..", "public");
-const app = express();
+var app = express();
+
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+  console.log('has a new connection!');
+
+  socket.on('disconnect', ()=>{
+    console.log('disconnect to the client.');
+  })
+});
 
 app.use(express.static(myPath));
-app.listen(port, ()=>{
+
+server.listen(port, ()=>{
   console.log('The system start on port', port);
 })
